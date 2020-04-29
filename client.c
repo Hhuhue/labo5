dd64a56bb7433e7b82323395a4fd930fe02a4481
root <root@localhost.localdomain>
2020-04-29 11:37:02 -0400

    Client sends data

--- client.c
+++ client.c
@@ -40,11 +40,23 @@ int main(int argc, char* argv[]) {
 	
 	int n, len; 
 	
+	sendto(sockfd, &header->channels, sizeof(unsigned int), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+	sendto(sockfd, &header->sample_rate, sizeof(unsigned int), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
 	sendto(sockfd, &header->size_of_each_sample, sizeof(long), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
 	sendto(sockfd, &header->num_samples, sizeof(long), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-    printf("Client header: size sample is %ld and number of samples is %ld\n", header->size_of_each_sample, header->num_samples); 
 	printf("Header infos sent.\n"); 
 
+    fprintf(stderr, "Starting read/write loop\n");
+    int total, read = 0;
+    char data_buffer[header->size_of_each_sample];
+    for (long i = 1; i <= header->num_samples; i++){
+        read = fread(data_buffer, sizeof(data_buffer), 1, file);
+	    sendto(sockfd, data_buffer, read, MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+    }    
+    fprintf(stderr, "End read/write loop\n");
+
 	close(sockfd); 
+    free(header);
+    fclose(file);
 	return 0; 
 } 
