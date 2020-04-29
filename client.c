c7878bd3745aebfcc1ff9bb7b5a99edb3321af89
root <root@localhost.localdomain>
2020-04-29 10:52:04 -0400

    Client sends header

--- client.c
+++ client.c
@@ -7,39 +7,41 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
+#include "wave.h"
 
 #define PORT	 8080 
 #define MAXLINE 1024 
 
 // Driver code 
-int main() { 
+int main(int argc, char* argv[]) { 
 	int sockfd; 
 	char buffer[MAXLINE]; 
 	char *hello = "Hello from client"; 
 	struct sockaddr_in servaddr; 
 
+    char *filename = argv[1];
+
+    FILE* file;
+    file = fopen(filename, "rb");
+    struct HEADER* header = init_file(file); 
+
 	// Creating socket file descriptor 
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
 		perror("socket creation failed"); 
 		exit(EXIT_FAILURE); 
 	} 
-
 
 	memset(&servaddr, 0, sizeof(servaddr)); 
 	
 	// Filling server information 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
-	servaddr.sin_addr.s_addr = inet_addr("192.168.2.73"); 
+	servaddr.sin_addr.s_addr = inet_addr(argv[2]); 
 	
 	int n, len; 
 	
-	sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-	printf("Hello message sent.\n"); 
-		
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len); 
-	buffer[n] = '\0'; 
-	printf("Server : %s\n", buffer); 
+	sendto(sockfd, header, sizeof(struct HEADER), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+	printf("Header sent.\n"); 
 
 	close(sockfd); 
 	return 0; 
